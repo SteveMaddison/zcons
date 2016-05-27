fd4c3d64dd5bf31be6cd33be487059c4c3409653
Steve Maddison <steve.maddison@alliander.com>
2016-05-27 16:29:53 +0200

    Write output file for tileconv.

--- tools/tileconv/main.c
+++ tools/tileconv/main.c
@@ -15,29 +15,33 @@ int main(int argc, char *argv[]) {
   SDL_Surface *in_surface = NULL;
   SDL_Surface *surface = NULL;
   SDL_PixelFormat *rgba;
-  const char *filename = argv[1];
+  const char *img_filename = argv[1];
+  const char *out_filename = argv[2];
   int cols = 0, rows = 0;
   int tile = 0;
   int px = 0, py = 0;
   int col = 0, row = 0;
   Uint8 tile_data[TILESET_SIZE*TILE_WIDTH*TILE_HEIGHT]; 
+  FILE *f = NULL;
+  int i = 0;
 
-  if (filename == NULL) {
-    fprintf(stderr, "No file name provided.\n");
+
+  if (argc < 3) {
+    fprintf(stderr, "Usage: %s IMAGE_FILE OUTPUT_FILE\n", argv[0]);
     return 1;
   }
 
-  in_surface = IMG_Load(filename);
+  in_surface = IMG_Load(img_filename);
 
   if (in_surface == NULL) {
-    fprintf(stderr, "Couldn't load image from %s\n", filename);
+    fprintf(stderr, "Couldn't load image from %s\n", img_filename);
     return 1;
   }
 
   cols = in_surface->w / TILE_WIDTH;
   rows = in_surface->h / TILE_HEIGHT;
 
-  printf("Read %s (%d x %d x %dbpp)\n", filename, in_surface->w, in_surface->h, in_surface->format->BitsPerPixel);
+  printf("Read %s (%d x %d x %dbpp)\n", img_filename, in_surface->w, in_surface->h, in_surface->format->BitsPerPixel);
   printf("Max %d x %d = %d tiles\n", cols, rows, cols * rows);
 
   rgba = SDL_AllocFormat(SDL_PIXELFORMAT_RGBA8888);
@@ -73,6 +77,14 @@ int main(int argc, char *argv[]) {
 
     tile++;
   }
+
+  f = fopen(out_filename, "wb");
+  if (f == NULL) {
+    printf("Error opening output file %s\n", out_filename);
+    exit(1);
+  }
+  fwrite(tile_data,sizeof(tile_data),1,f);
+  fclose(f);
 
   return 0;
 }
